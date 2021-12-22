f6e4bc9e74256ed7cc0db7602c3646586f5cd7c6
Ray's Git <lancelot53.ra@gmail.com>
2021-12-22 18:57:10 -0500

    Completed

--- lib/values.rb
+++ lib/values.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require "pry"
 
 #This is the groceries hash we'll be passing in to the get_the_values method
 # groceries = {
@@ -9,5 +9,16 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
+  arr =[]
   #code your solution here!
+  groceries.each do |key, values|
+    arr << values
+    arr.flatten
+    #binding.pry
+  end
+   arr.flatten
+  
 end
+
+#binding.pry
+#end
